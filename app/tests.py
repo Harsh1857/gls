da3c7720a41ad82b946d5bbd34f789598c9b57f9
Harsh <harsh.gupta@daffodilsw.com>
2019-05-18 13:25:44 +0530

    Adding a test case

--- app/tests.py
+++ app/tests.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import Home
 
 # Create your tests here.
+
+class TestHomeGlsPage(TestCase):
+    def  setUp(self):
+        self.factory = RequestFactory()
+
+    def test_gls_home_page_lending(self):
+        request = self.factory.get('/')
+        response = Home.as_view()(request)
+        self.assertEqual(response.status_code, 200)
